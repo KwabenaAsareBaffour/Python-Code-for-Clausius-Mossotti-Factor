01a5a76c11c1fcd4c1b5498b5bd0d4dfc861fe5c
Kwabena Asare Baffour <70045541+KwabenaAsareBaffour@users.noreply.github.com>
2020-08-30 14:37:23 -0400

    Update Clausius Mossotti Factor.py

--- Clausius Mossotti Factor.py
+++ Clausius Mossotti Factor.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-g = 2 * math.pi * np.arange(1000, 10000000, 10000)
-z = np.linspace(0.0002, 1, 20)
+g = 2 * math.pi * np.arange(1000, 100000000, 10000)
+z = np.linspace(0.0002, 1, 4)
 perm_of_free_space = (8.85418782 * pow(10, -12))
 
 perm_of_medium = 80 * perm_of_free_space
 
 
-perm_of_particle = 2.56 * perm_of_free_space 
-con_of_particle = (7 *  pow(10, -5))
+perm_of_particle = 2.33 * perm_of_free_space 
+con_of_particle = (0.053)
 
 for i in z:
     
@@ -25,14 +25,27 @@ for i in z:
     #The Clausius Mossoti Factor is determined by the following formula
     cm_factor = (complex_perm_of_particle - complex_perm_of_medium)/(complex_perm_of_particle + (2*complex_perm_of_medium))
     
-    x_axis = [x.real for x in cm_factor]
-    y_axis = [y.imag for y in cm_factor]
+    cm_factor_real = [x.real for x in cm_factor]
+    cm_factor_imag = [y.imag for y in cm_factor]
     
+    if i==z[0]:
+        try:
+            zero_val_index=cm_factor_real.index(min(map(abs,cm_factor_real)))
+        except:
+            zero_val_index=cm_factor_real.index(-1*min(map(abs,cm_factor_real)))
+    crossover=g[zero_val_index]
+        
     print(complex_perm_of_particle - complex_perm_of_medium)
     plt.xscale('log')
     
-    #plt.plot(g,y_axis)
-    plt.plot(g,x_axis)
-    plt.xlabel("Frequency")
-    plt.ylabel("CM Factor")
+    #plt.plot(g,cm_factor_imag)
+    plt.plot(g,cm_factor_real)
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Re(CMF[ω]")
     #plt.show()
+plt.plot([crossover]*50,np.linspace(-0.9,1,50),linestyle='--')
+legend=["{} S/m".format(i) for i in z]
+legend.append("Crossover Frequency: {} MHz".format(round(crossover/1000000,7)))
+plt.legend(legend)
+plt.grid(color='r', linestyle='--', linewidth=0.3)
+print("The Crossover Frequency is",crossover/1000000,"MHz")
